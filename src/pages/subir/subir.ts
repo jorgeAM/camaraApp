ded634f042d57c6c6caccd3c60b7073cc118e282
Jorge L <jorge.alfmur@gmail.com>
2018-01-20 11:14:08 -0500

    subida de archivo a firebase

--- src/pages/subir/subir.ts
+++ src/pages/subir/subir.ts
@@ -3,6 +3,8 @@ import { IonicPage, ViewController } from 'ionic-angular';
 //PLUGINS
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker';
+//PROVIDER
+import { CargaArchivoProvider } from "../../providers/carga-archivo/carga-archivo";
 
 @IonicPage()
 @Component({
@@ -12,11 +14,13 @@ import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker';
 export class SubirPage {
   titulo: string;
   image: string;
+  imagen64: string;
 
   constructor(
     public viewCtrl: ViewController,
     private camera: Camera,
-    private imagePicker: ImagePicker
+    private imagePicker: ImagePicker,
+    private cargaArchivoServicio: CargaArchivoProvider
   ) { }
 
   dismiss(){
@@ -33,6 +37,7 @@ export class SubirPage {
     this.imagePicker.getPictures(opciones).then((results) => {
       for (var i = 0; i < results.length; i++) {
         this.image = `data:image/jpeg;base64,${results[i]}`;
+        this.imagen64 = results[i];
       }
     }, (err) => console.log(err) );
   }
@@ -47,8 +52,25 @@ export class SubirPage {
 
     this.camera.getPicture(opciones).then((imageData) => {
       this.image = `data:image/jpeg;base64,${imageData}`;
+      this.imagen64 = imageData;
       console.log(this.image);
     }).catch(err => console.log("error: ", JSON.stringify(err)));
+  }
+
+  uploadImagen(){
+    let archivo = {
+      img: this.imagen64,
+      titulo: this.titulo
+    }
+    this.cargaArchivoServicio.cargarImagenFirebase(archivo);
+  }
+
+  uploadImagenAlternativo(){
+    let archivo = {
+      img: this.imagen64,
+      titulo: this.titulo
+    }
+    this.cargaArchivoServicio.cargarImagenAlternativo(archivo);
   }
 
 }
